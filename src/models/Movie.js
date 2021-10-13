cd71030dc79c5f4fddd06b14197a8dd19f41d231
Jake Overall <joverall22@gmail.com>
2021-10-12 18:20:34 -0600

    id first

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,10 +1,10 @@
 export class Movie {
   constructor(data) {
+    this.id = data.id
     this.title = data.title
     this.score = data.popularity
     this.img = data.poster_path ? 'https://image.tmdb.org/t/p/w500' + data.poster_path : 'https://davidkoepp.com/wp-content/themes/blankslate/images/Movie%20Placeholder.jpg'
     this.poster2 = data.backdrop_path ? 'https://image.tmdb.org/t/p/w500' + data.backdrop_path : 'https://www.kindpng.com/picc/m/18-189751_movie-placeholder-hd-png-download.png'
     this.overview = data.overview
-    this.id = data.id
   }
 }
